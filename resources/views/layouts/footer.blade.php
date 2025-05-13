8773489b2c033d276a34370980b89932bfdc7a56
Ryanrestuk04 <ryanrestuk734@gmail.com>
2025-05-14 03:06:33 +0700

    perubahanpertama

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="sticky-footer bg-white">
+    <div class="container-fluid my-auto">
+        <div class="copyright text-center my-auto">
+            <span>Copyright &copy; DESA RYAN {{ date('Y') }}</span>
+        </div>
+    </div>
+</footer>
